e415740e41066095632f486ae0b419623bfc1a0d
Masum Billah <office.hellomasum@gmail.com>
2025-02-14 14:39:31 +0600

    Updated

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,31 @@
 <footer class="ei-footer">
     <div class="container">
         <div class="ei-footer-container">
-            <div class="ei-footer-logo">Elite+</div>
-            <div class="ei-footer-subscribe">
-                <p>Receive our tips for a good night's sleep and our latest news by subscribing to our newsletter.</p>
-                <form class="ei-footer-form">
-                    <input type="text" placeholder="Last Name*">
-                    <input type="text" placeholder="First Name*">
-                    <input type="email" placeholder="Email*">
-                    <button class="ei-button">
-                        <span>SUBMIT</span>
-                        <div class="ei-arrow"></div>
-                    </button>
-                </form>
+            <div class="ei-footer-logo">
+                <a href="/">
+                    <img src="assets/img/logo.png" alt="Elite Israel">
+                </a>
+            </div>
+            <div class="ei-footer-contact">
+                <h3>Receive our tips for a good night's sleep and our latest news by subscribing to our newsletter.</h3>
+                <div class="ei-form-container">
+                    <div class="ei-form-group">
+                        <div class="ei-form-field">
+                            <label for="last-name">Last Name*</label>
+                            <input type="text" id="last-name" placeholder="">
+                        </div>
+                        <div class="ei-form-field">
+                            <label for="first-name">First Name*</label>
+                            <input type="text" id="first-name" placeholder="">
+                        </div>
+                    </div>
+                    <div class="ei-form-group">
+                        <div class="ei-form-field ei-full-width">
+                            <label for="email">Email*</label>
+                            <input type="email" id="email" placeholder="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="ei-footer-bottom-first">
@@ -24,7 +37,7 @@
             </div>
         </div>
         <div class="ei-footer-bottom-last">
-            <div class="ei-footer-info">Elite SA, En Roveray 198, 1170 Aubonne, CH. Tel: +41 21 821 17 20</div>
+            <div class="ei-footer-info">Elite Israel, 25/4 Ein Ha – Kore St. Rishon Le Zion, Israel 7527131, Tel: <a href="tel:+972 50-484-8558">+972 50-484-8558</a></div>
             <div class="ei-copyright-info">© 2025 — elitebeds.ch</div>
         </div>
     </div>
